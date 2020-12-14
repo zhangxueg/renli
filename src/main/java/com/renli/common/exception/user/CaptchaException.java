4c23c9bca05fd70ba9b3f62b4e87baa97e2e8d1c
zhangxg <577516964@qq.com>
2020-12-14 14:18:38 +0800

    初始化项目

---
+++ src/main/java/com/renli/common/exception/user/CaptchaException.java
@@ -0,0 +1,16 @@
+package com.renli.common.exception.user;
+
+/**
+ * 验证码错误异常类
+ * 
+ * @author zhangxuegang
+ */
+public class CaptchaException extends UserException
+{
+    private static final long serialVersionUID = 1L;
+
+    public CaptchaException()
+    {
+        super("user.jcaptcha.error", null);
+    }
+}
